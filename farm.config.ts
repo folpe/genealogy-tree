d6d074817f55e5dbd87541187e5ca0f4f7580399
Florent PELLEGRIN <dev@folpe.me>
2025-03-24 02:42:03 +0100

    fix(d3): add external dependency

--- farm.config.ts
+++ farm.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from '@farmfe/core'
 import farmPluginPostcss from '@farmfe/js-plugin-postcss'
 
 export default defineConfig({
+  compilation: {
+    external: ['d3'],
+    output: {
+      format: 'esm',
+    },
+  },
   // ... ignore other fields
   plugins: ['@farmfe/plugin-react', farmPluginPostcss()],
 })
